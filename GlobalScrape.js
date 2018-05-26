1468252cc8f67a62808715d24c9536ab25048da4
Jordan Schaller <JSchaller@student.fullsail.edu>
2018-05-26 01:27:56 -0400

    GlobalScrape.js v0.3

--- GlobalScrape.js
+++ GlobalScrape.js
@@ -7,39 +7,42 @@ request.onreadystatechange = function ()
 	if(request.readyState === 4 && request.status === 200) 
 	{
 		console.log("request to feed successfull");
+		var getFirst = request.responseText
 		var channelLinksString = getFirst.split("yt-simple-endpoint style-scope ytd-channel-renderer");
 
-		var videoCode = videos[0];
-		var videoMeteData = = meta[0];
+		//var videoCode = videoCode[0];
+		//var videoMeteData = videoMetaData[0];
 		
-		for (var i = 0; i < channelLinksString.length; i++) {
-		//get page data for each subscriber NEEDS TARGET
-		var videoInfoTxt = document.getElementsByTagName('html')[0].innerHTML
-		var dataArray = getFirst.split("style-scope ytd-section-list-renderer>");
-		//for each video on the page
-		var ChannelHMDLtag = /(.*video-title=\"\s+)(.*)(\s+>.*)/;
-		for (var videoNumb = 0; i < dataArray.length; i++)
+		for (var i = 0; i < channelLinksString.length; i++) 
 		{
-			videoData[i].videoCode[videoNumb].push(dataArray[videoNumb]);
-			videoData[i].videoMeteData[videoNumb].push(videoData[i].videoCode[videoNumb].replace(ChannelHMDLtag, "$2"));
-		}
-}
-		var URL = "https://www.youtube.com/channel/UCAql2DyGU2un1Ei2nMYsqOA/videos"; // REPACED WITH PARSED URLS
-		var requestch = new XMLHttpRequest();
-		requestch.open("GET", URL);
-		requestch.onreadystatechange = function () 
-		{
-			if(request.readyState === 4 && request.status === 200) 
+			//var URL = "https://www.youtube.com/channel/UCAql2DyGU2un1Ei2nMYsqOA/videos"; // REPACED WITH PARSED URLS
+			console.log(channelLinksString[i])
+			var requestch = new XMLHttpRequest();
+			requestch.open("GET", channelLinksString[i]);
+			requestch.onreadystatechange = function () 
 			{
-				console.log("request to feed successfull");
-				console.log(request.responseText);
+				if(request.readyState === 4 && request.status === 200) 
+				{
+					console.log("request to feed successfull");
+					//get page data for each subscriber NEEDS TARGET
+					var videoInfoTxt = document.getElementsByTagName('html')[0].innerHTML
+					var dataArray = getFirst.split("style-scope ytd-section-list-renderer>");
+					//for each video on the page
+					var ChannelHMDLtag = /(.*video-title=\"\s+)(.*)(\s+>.*)/;
+					for (var videoNumb = 0; i < dataArray.length; i++)
+					{
+						//videoData[i].videoCode[videoNumb].push(dataArray[videoNumb]);
+						//videoData[i].videoMeteData[videoNumb].push(videoData[i].videoCode[videoNumb].replace(ChannelHMDLtag, "$2"));
+					}
+				}
 			}
+			requestch.send();
 		}
-		requestch.send();
+
 	}
 }
 request.send();
-
+console.log("Finished");
 /*
 //get data from page still NEEDS TARGET
 var getFirst = document.getElementsByTagName('html')[0].innerHTML;
