1b7f5481e3ab5ef90334dd87fd76f432dc6680fd
mesmichael <meschaller22@gmail.com>
2018-05-25 23:06:20 -0400

    added a WaitingForSubPage.js script that waits for the correct page to be loaded before trying to change all the data in the page, in this case the page is https://www.youtube.com/feed/subscriptions
    the changes will be made to the sub feed to allow for the correct feed to be displayed.

---
+++ WaitingForSubPage.js
@@ -0,0 +1,12 @@
+<script>
+//script is used to change the Subscriptions page
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) 
+  {
+	if (window.location.href == "https://www.youtube.com/feed/subscriptions");
+	{
+		// do your things
+	}
+  }
+})
+</script>
