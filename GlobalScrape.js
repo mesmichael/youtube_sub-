0f7ca45389cb5eadeda46b76725ebae50d88c902
mesmichael <meschaller22@gmail.com>
2018-05-26 00:48:02 -0400

    fix

--- GlobalScrape.js
+++ GlobalScrape.js
@@ -7,8 +7,24 @@ request.onreadystatechange = function ()
 	if(request.readyState === 4 && request.status === 200) 
 	{
 		console.log("request to feed successfull");
-		
-		
+		var channelLinksString = getFirst.split("yt-simple-endpoint style-scope ytd-channel-renderer");
+
+		var videoData[] = {
+		videoCode[],
+		videoMeteData[],
+		};
+		for (var i = 0; i < channelLinksString.length; i++) {
+		//get page data for each subscriber NEEDS TARGET
+		var videoInfoTxt = document.getElementsByTagName('html')[0].innerHTML
+		var dataArray = getFirst.split("style-scope ytd-section-list-renderer>");
+		//for each video on the page
+		var ChannelHMDLtag = /(.*video-title=\"\s+)(.*)(\s+>.*)/;
+		for (var videoNumb = 0; i < dataArray.length; i++)
+		{
+			videoData[i].videoCode[videoNumb].push(dataArray[videoNumb]);
+			videoData[i].videoMeteData[videoNumb].push(videoData[i].videoCode[videoNumb].replace(ChannelHMDLtag, "$2"));
+		}
+}
 		var URL = "https://www.youtube.com/channel/UCAql2DyGU2un1Ei2nMYsqOA/videos"; // REPACED WITH PARSED URLS
 		var requestch = new XMLHttpRequest();
 		requestch.open("GET", URL);
